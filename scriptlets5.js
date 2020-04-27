91ee7006145b202a1d58044873cd56ba25f6ad14
James Anto <semaj.anto@gmail.com>
2020-04-28 00:43:45 +0530

    Update and rename scriptlets4.js to scriptlets5.js

--- scriptlets5.js
+++ scriptlets5.js
@@ -2,7 +2,9 @@
 /// alias ds.js
 (function() {
     let needle1 = '{{1}}'
+    console.log('needle 1', needle1);
     let keyCodes = Array.from(needle1.split('\,').map(e => e.trim()).filter(e => e !== '').map(e => parseInt(e)));
+    console.log('kc', keyCodes);
     document.addEventListener("keydown", function(e) {
         if(keyCodes.indexOf(e.keyCode) !== -1) {
             console.log("EEE", e);
