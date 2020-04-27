2ff7d4ad8adc655cc2bc739fc8d7b37d4a93df45
James Anto <semaj.anto@gmail.com>
2020-04-28 00:46:26 +0530

    Update and rename scriptlets5.js to scriptlets6.js

--- scriptlets6.js
+++ scriptlets6.js
@@ -3,7 +3,7 @@
 (function() {
     let needle1 = '{{1}}'
     console.log('needle 1', needle1);
-    let keyCodes = Array.from(needle1.split('\,').map(e => e.trim()).filter(e => e !== '').map(e => parseInt(e)));
+    let keyCodes = Array.from(needle1.split('\|').map(e => e.trim()).filter(e => e !== '').map(e => parseInt(e)));
     console.log('kc', keyCodes);
     document.addEventListener("keydown", function(e) {
         if(keyCodes.indexOf(e.keyCode) !== -1) {
